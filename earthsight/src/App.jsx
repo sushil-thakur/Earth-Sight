2a9fa0aba6a4c77f8b297eb3f9de994df0fefef3
Sushil <sushilt2059@gmail.com>
2025-10-04 22:28:59 +0545

    new ui fro home page more to add

---
+++ earthsight/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Hero from './components/Hero'
+import About from './components/About'
+import Navbar from './components/Navbar'
+import Features from './components/Features'
+import Story from './components/Story'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import ImageSlider from './components/ImageSlider'
+
+
+const App = () => {
+  return (
+   <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <Navbar/>
+    <Hero/>
+   <About/>
+   <ImageSlider/>
+   <Features/>
+   <Story/>
+   <Contact/>
+   <Footer/>
+   
+   </main>
+  )
+}
+
+export default App
