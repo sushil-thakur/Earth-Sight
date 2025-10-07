e51e35dbcfd88df8f4ad6cee8ab150c47e97336f
Sushil <sushilt2059@gmail.com>
2025-10-07 21:11:49 +0545

    deforestion page added more detail to added

--- earthsight/src/pages/Home.jsx
+++ earthsight/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import Hero from '../components/Hero'
+
 import About from '../components/About'
 import ImageSlider from '../components/ImageSlider'
 import Features from '../components/Features'
 import Story from '../components/Story'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
+import Hero from '../components/HEro'
 
 const Home = () => {
   return (
