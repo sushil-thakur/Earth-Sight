7dfa2ed0c554455695da00cf380439fcc6b73fa8
Sushil <sushilt2059@gmail.com>
2025-11-02 22:29:42 +0545

    rebder deploy

--- earthsight/src/pages/Home.jsx
+++ earthsight/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Features from '../components/Features'
 import Story from '../components/Story'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
-import Hero from '../components/HEro'
+import Hero from '../components/Hero'
 // import Laser from '@/components/laser'
 
 const Home = () => {
